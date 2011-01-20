ba3e9d9df1085b2b9ef49151fa808be1af8c8125
vagrant <vagrant@vagrant-ubuntu.hsd1.ca.comcast.net>
2011-01-19 23:01:21 -0800

    Completed the signup task

--- signup/hello_ruby.rb
+++ signup/hello_ruby.rb
@@ -1,7 +1,7 @@
 # Put your name (or username/alias) between the quotes
-name = ""
+name = "Arnie Nelson"
 
 # Put your favorite fruit between the quotes
-favorite_fruit = ""
+favorite_fruit = "Tangerine"
 
 puts "Hi!  My name is #{name} and my favorite fruit is #{favorite_fruit}"
